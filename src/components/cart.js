e3d5243d052b9d6d1a56a298e56a6956b3aacf47
Imlalitbishtt <lbist93@gmail.com>
2022-03-27 23:44:20 +0530

    Initial commit

---
+++ src/components/cart.js
@@ -0,0 +1,52 @@
+ import React, { useState, useEffect } from "react";
+import "../styles/Cart.css";
+
+const Cart = ({ cart, setCart, handleChange }) => {
+  const [price, setPrice] = useState(0);
+
+  const handleRemove = (id) => {
+    const arr = cart.filter((item) => item.id !== id);
+    setCart(arr);
+    handlePrice();
+  };
+
+  const handlePrice = () => {
+    let ans = 0;
+    cart.map((item) => (ans += item.amount * item.price));
+    setPrice(ans);
+  };
+
+  useEffect(() => {
+    handlePrice();
+  });
+
+  return (
+    <article>
+      {cart.map((item) => (
+        <div className="cart_box" key={item.id}>
+          <div className="cart_img">
+            <img src={item.img} alt="" />
+            <p>{item.title}</p>
+            <span className="price">₹{item.price}</span>
+          </div>
+          
+          <div className="btn">
+            <button className="btn" onClick={() => handleChange(item, 1)}>+</button>
+            <button className="btn">{item.amount}</button>
+            <button className="btn" onClick={() => handleChange(item, -1)}>-</button>
+          </div>
+          <div>
+            <span className="total-price">{item.price*item.amount}</span>
+            <button className="remove" onClick={() => handleRemove(item.id)}>Remove</button>
+          </div>
+        </div>
+      ))}
+      <div className="total">
+        <span>Total Price of your Cart</span>
+        <span>₹{price}</span>
+      </div>
+    </article>
+  );
+};
+
+export default Cart;
